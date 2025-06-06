e824aeaf1765522d794569c0c230f88e8e1fa573
DESKTOP-BK99AFT\hjin3 <hjin3456@naver.com>
2025-06-06 16:57:20 +0900

    복습하면서 수정한 부분

--- back_workspace/java_workspace2/threadapp/src/main/java/com/sinse/threadapp/ani/ProgressThread.java
+++ back_workspace/java_workspace2/threadapp/src/main/java/com/sinse/threadapp/ani/ProgressThread.java
@@ -13,11 +13,14 @@ public class ProgressThread extends Thread{
 	}
 	
 	public void run() {
-		while(n<=100) { //진행바가 100에 도달하면 종료	
+		while(true) { //진행바가 100에 도달하면 종료	
 			try {
 				Thread.sleep(10);
-				bar.setValue(n);
+				//if (n > 100) break; 
+	            //if를 앞에 둘 거면 n > 100으로 해야 100까지 보여주고 멈출 수 있음
 				n+=velX;
+				bar.setValue(n);
+				if (n >= 100) break; // 100을 넘으면 루프 종료 (종료 조건)
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 				break; //예외발생 시 스레드 종료
